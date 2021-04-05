bf881905a1723c5b19a20c3e5c557fa024cc03be
nemanja-z <nemanja14zivanovic@gmail.com>
2021-04-05 18:46:46 +0200

    Add fields for title and description

--- admin/partials/meta-box-post-settings.php
+++ admin/partials/meta-box-post-settings.php
@@ -1,13 +1,13 @@
 <?php
-$lnpw_enabled        = get_post_meta( $post->ID, 'lnpw_enabled', true );
-$lnpw_price          = get_post_meta( $post->ID, 'lnpw_price', true );
-$lnpw_duration       = get_post_meta( $post->ID, 'lnpw_duration', true );
-$lnpw_duration_type  = get_post_meta( $post->ID, 'lnpw_duration_type', true );
+$lnpw_enabled        = get_post_meta($post->ID, 'lnpw_enabled', true);
+$lnpw_price          = get_post_meta($post->ID, 'lnpw_price', true);
+$lnpw_duration       = get_post_meta($post->ID, 'lnpw_duration', true);
+$lnpw_duration_type  = get_post_meta($post->ID, 'lnpw_duration_type', true);
 $supported_durations = Lightning_Paywall_Admin::DURATIONS;
-$used_currency       = get_option( 'lnpw_currency' );
-$used_price          = get_option( 'lnpw_default_price' );
-$used_duration_type  = get_option( 'lnpw_default_duration_type' );
-$used_duration       = get_option( 'lnpw_default_duration' );
+$used_currency       = get_option('lnpw_currency');
+$used_price          = get_option('lnpw_default_price');
+$used_duration_type  = get_option('lnpw_default_duration_type');
+$used_duration       = get_option('lnpw_default_duration');
 
 ?>
 
@@ -19,7 +19,7 @@ $used_duration       = get_option( 'lnpw_default_duration' );
     </div>
 
     <div style="margin-top: 25px;">
-        <input type="number" min=0 placeholder="Price" step=<?php echo $used_currency === 'BTC' ? '0.00000001':( $used_currency === 'SATS' ? '1' : '0.50' );?> name="lnpw_price" value="<?php echo $lnpw_price ? $lnpw_price : '' ?>">
+        <input type="number" min=0 placeholder="Price" step=<?php echo $used_currency === 'BTC' ? '0.00000001' : ($used_currency === 'SATS' ? '1' : '0.50'); ?> name="lnpw_price" value="<?php echo $lnpw_price ? $lnpw_price : '' ?>">
         <p>If field empty, used default value - <?php echo "{$used_price} {$used_currency}"; ?></p>
     </div>
 
@@ -30,12 +30,29 @@ $used_duration       = get_option( 'lnpw_default_duration' );
         <select name="lnpw_duration_type">
             <option disabled value="">Select duration type</option>
             <option value="">Default</option>
-			<?php foreach ( $supported_durations as $duration ) : ?>
-                <option <?php selected( $lnpw_duration, $duration ); ?> value="<?php echo $duration ?>"><?php echo $duration ?></option>
-			<?php endforeach; ?>
+            <?php foreach ($supported_durations as $duration) : ?>
+                <option <?php selected($lnpw_duration, $duration); ?> value="<?php echo $duration ?>"><?php echo $duration ?></option>
+            <?php endforeach; ?>
 
         </select>
         <p>If field 'default', used default value - <?php echo $used_duration_type; ?></p>
+    </div>
+
+    <div>
+        <div>
+            <label for="checkout_view_title">View title</label>
+        </div>
+        <div>
+            <textarea id="checkout_view_title" name=""><?php   ?></textarea>
+        </div>
+    </div>
+    <div>
+        <div>
+            <label for="checkout_view_description">View description</label>
+        </div>
+        <div>
+            <textarea id="checkout_view_description" name=""><?php   ?></textarea>
+        </div>
     </div>
 
 </div>
