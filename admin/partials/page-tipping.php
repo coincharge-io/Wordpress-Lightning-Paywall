44157bd45047ed709e7da7e576754f9c9ab035bc
nemanja-z <nemanja14zivanovic@gmail.com>
2021-05-21 14:17:33 +0200

    Add additional donor information field

--- admin/partials/page-tipping.php
+++ admin/partials/page-tipping.php
@@ -120,6 +120,56 @@ $collect = get_option('lnpw_tipping_collect');
 
             </div>
         </div>
+        <?php if ($collect === 'true') : ?>
+            <div class="container-donor-information">
+                <div class="row">
+                    <div class="col-20">
+                        <label for="lnpw_tipping_collect_name">Full name</label>
+                    </div>
+                    <div class="col-80">
+                        <input type="hidden" id="lnpw_tipping_collect_name" class="lnpw_tipping_collect_name" name="lnpw_tipping_collect_name" value='false' />
+                        <input type="checkbox" id="lnpw_tipping_collect_name" class="lnpw_tipping_collect_name" name="lnpw_tipping_collect_name" <?php echo $collect_name === 'true' ? 'checked' : ''; ?> value="true" />
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-20">
+                        <label for="lnpw_tipping_collect_email">Email</label>
+                    </div>
+                    <div class="col-80">
+                        <input type="hidden" id="lnpw_tipping_collect_email" class="lnpw_tipping_collect_email" name="lnpw_tipping_collect_email" value='false' />
+                        <input type="checkbox" id="lnpw_tipping_collect_email" class="lnpw_tipping_collect_email" name="lnpw_tipping_collect_email" <?php echo $collect_name === 'true' ? 'checked' : ''; ?> value="true" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-20">
+                        <label for="lnpw_tipping_collect_address">Address</label>
+                    </div>
+                    <div class="col-80">
+                        <input type="hidden" id="lnpw_tipping_collect_address" class="lnpw_tipping_collect_address" name="lnpw_tipping_collect_address" value='false' />
+                        <input type="checkbox" id="lnpw_tipping_collect_address" class="lnpw_tipping_collect_address" name="lnpw_tipping_collect_address" <?php echo $collect_name === 'true' ? 'checked' : ''; ?> value="true" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-20">
+                        <label for="lnpw_tipping_collect_phone">Phone number</label>
+                    </div>
+                    <div class="col-80">
+                        <input type="hidden" id="lnpw_tipping_collect_phone" class="lnpw_tipping_collect_phone" name="lnpw_tipping_collect_phone" value='false' />
+                        <input type="checkbox" id="lnpw_tipping_collect_phone" class="lnpw_tipping_collect_phone" name="lnpw_tipping_collect_phone" <?php echo $collect_name === 'true' ? 'checked' : ''; ?> value="true" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-20">
+                        <label for="lnpw_tipping_collect_message">Message</label>
+                    </div>
+                    <div class="col-80">
+                        <input type="hidden" id="lnpw_tipping_collect_message" class="lnpw_tipping_collect_message" name="lnpw_tipping_collect_message" value='false' />
+                        <input type="checkbox" id="lnpw_tipping_collect_message" class="lnpw_tipping_collect_message" name="lnpw_tipping_collect_message" <?php echo $collect_name === 'true' ? 'checked' : ''; ?> value="true" />
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
         <div style="display: inline-block; margin-top: 25px;">
             <button class="button button-primary" type="submit">Save</button>
         </div>
